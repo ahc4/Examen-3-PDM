2a4fd6982910dc8e37e1195177a5e57bccba7fea
Antonio Hernández Castilleja <antonio.hernandezcastilleja@outlook.com>
2021-11-18 20:51:49 -0600

    La clase y layout de Buscar modificados.

--- app/src/main/java/com/ahc/examen3/Buscar.java
+++ app/src/main/java/com/ahc/examen3/Buscar.java
@@ -23,7 +23,6 @@ public class Buscar extends AppCompatActivity implements Response.Listener<JSONO
     EditText nombre;
     EditText costo;
     EditText foto;
-    EditText search;
     Button buscar;
     String url;
 
@@ -39,14 +38,13 @@ public class Buscar extends AppCompatActivity implements Response.Listener<JSONO
         nombre = findViewById(R.id.editTextTextPersonName);
         costo = findViewById(R.id.editTextNumber5);
         foto = findViewById(R.id.editTextTextPersonName4);
-        search = findViewById(R.id.editTextNumber3);
         buscar = findViewById(R.id.button2);
         request = Volley.newRequestQueue(getApplicationContext());
 
         buscar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                url = "http://serviciosdigitalesplus.com/catalogo.php?tipo=1&id=" + buscar.getText().toString();
+                url = "http://serviciosdigitalesplus.com/catalogo.php?tipo=1&id=" + id.getText().toString();
                 btnClick(view);
             }
         });
@@ -75,7 +73,7 @@ public class Buscar extends AppCompatActivity implements Response.Listener<JSONO
         }
         catch (Exception e)
         {
-
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
         //Toast.makeText(this, "OK", Toast.LENGTH_SHORT).show();
     }
