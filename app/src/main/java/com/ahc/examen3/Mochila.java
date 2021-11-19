4793bd89fe2bee79a5f46fad410a0763b1ba379e
Antonio Hernández Castilleja <antonio.hernandezcastilleja@outlook.com>
2021-11-18 21:31:22 -0600

    Nueva clase: Mochila

---
+++ app/src/main/java/com/ahc/examen3/Mochila.java
@@ -0,0 +1,21 @@
+package com.ahc.examen3;
+
+public class Mochila {
+    int id;
+    String nombre;
+    int costo;
+    String foto;
+
+    public Mochila(int id, String nombre, int costo, String foto)
+    {
+        this.id = id;
+        this.nombre = nombre;
+        this.costo = costo;
+        this.foto = foto;
+    }
+
+    public Mochila()
+    {
+
+    }
+}
