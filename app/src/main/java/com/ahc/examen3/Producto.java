7677bbf39b8ff10cd716e173883e73e9229db792
Antonio Hernández Castilleja <antonio.hernandezcastilleja@outlook.com>
2021-11-18 23:53:15 -0600

    Mochila a Producto

---
+++ app/src/main/java/com/ahc/examen3/Producto.java
@@ -0,0 +1,20 @@
+package com.ahc.examen3;
+
+public class Producto
+{
+    int id, costo;
+    String nombre, foto;
+
+    public Producto(int id, String nombre, int costo, String foto)
+    {
+        this.id = id;
+        this.nombre = nombre;
+        this.costo = costo;
+        this.foto = foto;
+    }
+
+    public Producto()
+    {
+
+    }
+}
